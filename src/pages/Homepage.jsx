249974e73487698087b57fc08099643e574f7ef0
Samiraazam <samira.azam1706@gmail.com>
2023-03-01 13:08:50 +0600

    tailwind install

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Homepage = () => {
   return (
     <div>
-      <h2>Homepage</h2>
+      <h2 className="text-3xl font-bold underline">Homepage</h2>
     </div>
   );
 };
