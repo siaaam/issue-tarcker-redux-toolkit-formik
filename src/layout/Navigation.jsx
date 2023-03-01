49c4422e2119a88b9ba02068f45866ea6c55c9bf
Samiraazam <samira.azam1706@gmail.com>
2023-03-01 13:36:40 +0600

    navbar designed with tailwind

--- src/layout/Navigation.jsx
+++ src/layout/Navigation.jsx
@@ -3,10 +3,23 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/issues">Issues</Link>
-      <Link to="/add-issue">Add Issue</Link>
+    <nav className="w-full bg-slate-200 flex justify-between items-center p-4">
+      <h1 className="font-bold text-lg">Issue Tracker</h1>
+      <ul className="flex justify-between items-center gap-5">
+        <li>
+          <Link className="hover:text-cyan-700" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="hover:text-cyan-700" to="/issues">
+            Issues
+          </Link>
+        </li>
+        <li className="bg-slate-400 px-4 py-2 rounded-md hover:bg-slate-700 hover:text-white">
+          <Link to="/add-issue">Add Issue</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
